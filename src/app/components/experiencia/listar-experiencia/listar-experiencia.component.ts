94ead32b610e0d30d07d962668923bf4733c9c8f
MiguelGuevara25 <miguel_g1789@hotmail.com>
2025-06-10 19:09:19 -0500

    add experiencia's table

---
+++ src/app/components/experiencia/listar-experiencia/listar-experiencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-experiencia',
+  imports: [],
+  templateUrl: './listar-experiencia.component.html',
+  styleUrl: './listar-experiencia.component.css'
+})
+export class ListarExperienciaComponent {
+
+}
