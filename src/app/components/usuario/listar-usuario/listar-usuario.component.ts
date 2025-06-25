a1fef6f059c7e3678672eaebd8f61b12ce7ddd30
PaToxCo <rodrigoanayaquispe1206@gmail.com>
2025-06-24 23:18:11 -0500

    Paginator

--- src/app/components/usuario/listar-usuario/listar-usuario.component.ts
+++ src/app/components/usuario/listar-usuario/listar-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Usuario } from '../../../models/usuario';
 import { UsuarioService } from '../../../services/usuario.service';
@@ -6,27 +6,34 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listar-usuario',
-  imports: [MatTableModule, CommonModule, RouterLink, MatIconModule, MatButtonModule],
+  imports: [MatTableModule, CommonModule, RouterLink, MatIconModule, MatButtonModule, MatPaginatorModule],
   templateUrl: './listar-usuario.component.html',
   styleUrl: './listar-usuario.component.css'
 })
-export class ListarUsuarioComponent implements OnInit {
+export class ListarUsuarioComponent implements OnInit, AfterViewInit {
 
   dataSource: MatTableDataSource<Usuario> = new MatTableDataSource()
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private usuServi: UsuarioService) { }
 
   ngOnInit(): void {
     this.usuServi.listar().subscribe((us_data) => {
       this.dataSource = new MatTableDataSource(us_data)
+      this.dataSource.paginator = this.paginator; // <== aquí lo asignas
+
     })
     this.usuServi.getList().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator; // <== aquí lo asignas
     });
+
   }
   eliminar(id: number) {
     this.usuServi.delete(id).subscribe((data) => {
@@ -34,6 +41,9 @@ export class ListarUsuarioComponent implements OnInit {
         this.usuServi.setList(data);
       });
     });
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 }
 
