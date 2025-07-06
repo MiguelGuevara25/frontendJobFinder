d7eedd13ef47aecbe1662f3fe4c0fccef53a5e50
G4TUNO <u202219154@upc.edu.pe>
2025-07-06 18:48:48 -0500

    Filtro de busqueda de centro de estudio

--- src/app/components/reportes/reportes.component.ts
+++ src/app/components/reportes/reportes.component.ts
@@ -4,15 +4,23 @@ import { CantidadcertificadosComponent } from './cantidadcertificados/cantidadce
 import { CantidadempresacursoComponent } from './cantidadempresacurso/cantidadempresacurso.component';
 import { CantidadplataformacursoComponent } from './cantidadplataformacurso/cantidadplataformacurso.component';
 import { MatCardModule } from '@angular/material/card';
-import { CurriculumpromedioComponent } from "./curriculumpromedio/curriculumpromedio.component";
+import { CurriculumpromedioComponent } from './curriculumpromedio/curriculumpromedio.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
-import { FrecuenciahabilidadesComponent } from "./frecuenciahabilidades/frecuenciahabilidades.component";
-
+import { FrecuenciahabilidadesComponent } from './frecuenciahabilidades/frecuenciahabilidades.component';
 
 @Component({
   selector: 'app-reportes',
-  imports: [MatTabsModule, MatExpansionModule, CantidadcertificadosComponent, CantidadempresacursoComponent, CantidadplataformacursoComponent, MatCardModule, CurriculumpromedioComponent, FrecuenciahabilidadesComponent],
+  imports: [
+    MatTabsModule,
+    MatExpansionModule,
+    CantidadcertificadosComponent,
+    CantidadempresacursoComponent,
+    CantidadplataformacursoComponent,
+    MatCardModule,
+    CurriculumpromedioComponent,
+    FrecuenciahabilidadesComponent,
+  ],
   templateUrl: './reportes.component.html',
   styleUrl: './reportes.component.css',
 })
